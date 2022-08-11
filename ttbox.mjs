4809f1e1e16eae65e913f997b0c785ed9e2da506
Jonathan Rock <Corgan.wh@gmail.com>
2022-08-10 21:12:34 -0700

    Blammo

--- ttbox.mjs
+++ ttbox.mjs
@@ -1,7 +1,5 @@
 const getToolbox = async function() {
-    let baseDir = document.getElementById('TTBox-Script').src;
-    baseDir = baseDir.slice(0, baseDir.indexOf('/ttbox.mjs'));
-    let toolboxWindow = (await (new Promise(resolve => nw.Window.getAll(a => resolve(a))))).find(w => w.window.location.href.match("/mods/ttbox.html"));
+    let toolboxWindow = (await (new Promise(resolve => nw.Window.getAll(a => resolve(a))))).find(w => w.window.location.href.match("/mods/ttbox/ttbox.html"));
 
     if(toolboxWindow === undefined) {
         toolboxWindow = await (new Promise((resolve) => {
@@ -16,9 +14,11 @@ const getToolbox = async function() {
 }
 
 const updateData = function() {
-    window.isSaving = true;
-    global.game_data = JSON.parse(JSON.stringify(game));
-    window.isSaving = false;
+    if(window.game) {
+        window.isSaving = true;
+        global.game_data = JSON.parse(JSON.stringify(window.game));
+        window.isSaving = false;
+    }
     requestAnimationFrame(updateData);
 }
 
